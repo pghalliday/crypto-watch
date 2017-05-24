7efff2890bb04f78a61425a011d776251acffd99
Peter Halliday <pghalliday@gmail.com>
2017-05-24 11:58:44 +0200

    rename to hodler

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# crypto-watch
+# Hodler
 
-[![Build Status](https://travis-ci.org/pghalliday/crypto-watch.svg?branch=master)](https://travis-ci.org/pghalliday/crypto-watch)
-[![Coverage Status](https://coveralls.io/repos/github/pghalliday/crypto-watch/badge.svg?branch=master)](https://coveralls.io/github/pghalliday/crypto-watch?branch=master)
+[![Build Status](https://travis-ci.org/pghalliday/hodler.svg?branch=master)](https://travis-ci.org/pghalliday/hodler)
+[![Coverage Status](https://coveralls.io/repos/github/pghalliday/hodler/badge.svg?branch=master)](https://coveralls.io/github/pghalliday/hodler?branch=master)
 
-Watch crypto-currency addresses.
+Watch assets.
 
 ## Contributing
 
--- predeploy.sh
+++ predeploy.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-sed -i.bak 's/"\/index/"\/crypto-watch\/index/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/"\/index/"\/hodler\/index/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
-sed -i.bak 's/"\/manifest/"\/crypto-watch\/manifest/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/"\/manifest/"\/hodler\/manifest/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
-sed -i.bak 's/"\/images/"\/crypto-watch\/images/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/"\/images/"\/hodler\/images/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
-sed -i.bak 's/\/service-worker/\/crypto-watch\/service-worker/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/\/service-worker/\/hodler\/service-worker/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
-sed -i.bak 's/"\/bower_components/"\/crypto-watch\/bower_components/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/"\/bower_components/"\/hodler\/bower_components/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
-sed -i.bak 's/"\/ui/"\/crypto-watch\/ui/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
+sed -i.bak 's/"\/ui/"\/hodler\/ui/g' build/default/index.html build/default/service-worker.js build/default/manifest.json
 rm build/default/*.bak
